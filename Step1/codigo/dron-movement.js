3fc6907056d17a63be2b760b1ed9fe00c3b0df7a
Javier Vázquez Moreno <javier.vazquez@qaracter.es>
2023-04-12 18:51:36 +0200

    update all (nothing>)

--- Step1/codigo/dron-movement.js
+++ Step1/codigo/dron-movement.js
@@ -1,28 +1,11 @@
 
 
 AFRAME.registerComponent('dron-movement', {
+    init: function(){
 
-    schema: {
-            acceleration: {default: 40},
-        },
-
-    init: function () {
-        var el = this.el;
-        var velocity = new THREE.Vector3();       
-              
     },
 
+    tick: function(time, deltaTime){
 
-    tick: function (time, delta) {
-      var dron = el.querySelector('#dron');
-        var bot_up = el.querySelector('#boton_arriba');
-        var velocity = this.velocity;
-        var posy = dron.object3D.position.y;
-        var target;
-
-        bot_up.addEventListener('click', function (event) {
-            pos += acceleration * delta;
-        });
-    },
-
+    }
 })
