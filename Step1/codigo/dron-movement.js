4e8932417acdc3863deb93757a355daa83894f3d
Javier Vázquez Moreno <javier.vazquez@qaracter.es>
2023-04-30 20:07:28 +0200

    update 2 step 1 30-04-2023

--- Step1/codigo/dron-movement.js
+++ Step1/codigo/dron-movement.js
@@ -1,29 +1,66 @@
-AFRAME.registerComponent('up', { 
+AFRAME.registerComponent('movement', { 
     schema: {
-        activado: {type: 'boolean', default: 'false'},
-        click: {type: 'int', default: 0}
-    },
-
-    init: function(){
-      this.el.object3D.position.y = 0.25;
+        activadoup: {type: 'boolean', default: 'false'},
+        activadodown: {type: 'boolean', default: 'false'},
+        clickup: {type: 'int', default: 0},
+        clickdown: {type: 'int', default: 0}
     },
 
     tick: function(){       
         var botonupComponent = document.querySelector('[botonup]').components.botonup;
-        
-        botonupComponent.el.addEventListener('click', (event)=>{
-           if(this.data.click == 0){
-                this.data.activado = true;
-                this.data.click = 1;
-           }else{
-                this.data.activado = false;
-                this.data.click = 0;
-           }            
+        var botondownComponent = document.querySelector('[botondown]').components.botondown;
+
+        console.log("ACTIVADO UP: "+this.data.activadoup + " - CLICK UP: "+ this.data.clickup + "| ACTIVADO DOWN: "+ this.data.activadodown +
+            " - CLICK DOWN: " + this.data.clickdown);
+
+        botonupComponent.el.addEventListener('click', (event)=>{            
+            
+           if(this.data.clickup == 0 && this.data.clickdown == 0){
+                console.log("TRAZA PARADO -> SUBIR");
+                this.data.activadoup = true;
+                this.data.clickup = 1;
+                this.data.activadodown = false;
+           }else if(this.data.clickup == 0 && this.data.clickdown == 1){
+                console.log("TRAZA BAJANDO -> SUBIR");
+                this.data.activadoup = true;
+                this.data.clickup = 1;
+                this.data.clickdown = 0;
+                this.data.activadodown = false;
+            
+           }else if(this.data.clickup == 1 && this.data.clickdown == 0){
+            console.log("TRAZA SUBIENDO -> PARAR");
+                this.data.activadoup = false;
+                this.data.clickup = 0;
+                this.data.activadodown = false;
+           }           
         })
 
-        if(this.data.activado){
+        botondownComponent.el.addEventListener('click', (event)=>{
+            
+            if(this.data.clickup == 0 && this.data.clickdown == 0){
+                console.log("TRAZA PARADO->BAJAR");
+                this.data.activadodown = true;
+                this.data.clickdown = 1;
+                this.data.activadoup = false;
+           }else if(this.data.clickup == 0 && this.data.clickdown == 1){
+            console.log("TRAZA BAJANDO -> PARAR");
+                this.data.activadodown = false;
+                this.data.clickdown = 0;
+                this.data.activadoup = false;
+           }else if(this.data.clickup == 1 && this.data.clickdown == 0){
+            console.log("TRAZA SUBIENDO -> BAJAR");
+                this.data.activadodown = true;
+                this.data.clickdown = 1;
+                this.data.activadoup = false;
+                this.data.clickup = 0;
+           }           
+         })
+
+        if(this.data.activadoup){
             this.el.object3D.position.y += 0.01;
-        } 
+        }else if(this.data.activadodown){
+            this.el.object3D.position.y -= 0.01;
+        }
     }
 }),
 
@@ -33,4 +70,12 @@ AFRAME.registerComponent('botonup', {
             console.log("El elemento boton_arriba ha sido CLICKEADO!");
         }
     },
+}),
+
+AFRAME.registerComponent('botondown', {
+    events: {
+        click: function(evt){
+            console.log("Elemento boton_abajo ha sido CLICKEADO");
+        }
+    }
 })
